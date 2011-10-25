85d285da0b150b718edce8310b48e56b5643c218
EmpowerApple <appledev@ecps.md>
2011-10-25 15:07:44 -0500

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 
   root :to => 'pages#home'
-  
-  root_path => '/'
-  root_url  => 'http://localhost:3000/'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
