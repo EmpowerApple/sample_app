3698d6bbcf11a6ad0ea8c37f7f281507a44d33c9
EmpowerApple <appledev@ecps.md>
2011-10-25 11:27:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '84d391788f46cd4885e9d2585e80a464026029fcbf2b90dec5a1094a670249c082add84830c9f58e97e34e92fcf120f5a21b3361b4bde454fe797ae09ae17518'
